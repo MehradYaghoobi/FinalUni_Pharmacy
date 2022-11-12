b100bc7cc2212c4bdf17f37a53c50490d8ac9b7d
MY <teach2049@gmail.com>
2022-11-12 13:25:47 +0330

    Init HomePage Ui & Added Slider HomePage

---
+++ Pharmacy/01_PharmacyQuery/Query/SlideQuery.cs
@@ -0,0 +1,37 @@
+﻿using _01_PharmacyQuery.Contracts.Slide;
+using ShopManagement.Infrastructure.EFCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_PharmacyQuery.Query
+{
+    public class SlideQuery : ISlideQuery
+    {
+        private readonly ShopContext _shopContext;
+
+        public SlideQuery(ShopContext shopContext)
+        {
+            _shopContext = shopContext;
+        }
+
+        public List<SlideQueryModel> GetSlides()
+        {
+            return _shopContext.Slides
+                .Where(x => x.IsRemoved == false)
+                .Select(x => new SlideQueryModel
+                {
+                    Picture = x.Picture,
+                    PictureAlt = x.PictureAlt,
+                    PictureTitle = x.PictureTitle,
+                    BtnText = x.BtnText,
+                    Heading = x.Heading,
+                    Link = x.Link,
+                    Text = x.Text,
+                    Title = x.Title
+                }).ToList();
+        }
+    }
+}
