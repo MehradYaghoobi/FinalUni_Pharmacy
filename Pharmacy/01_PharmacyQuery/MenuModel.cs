f37b2c734b99c7f330309a5f32b5b86ca9f9a624
MY <teach2049@gmail.com>
2022-11-14 13:13:57 +0330

    Fixed Menu On Home Page & In the future, I need to make some changes to the way the latest products are selected and the product categories on the home page & I should have a section called category titles to better display product categories

---
+++ Pharmacy/01_PharmacyQuery/MenuModel.cs
@@ -0,0 +1,16 @@
+﻿using _01_PharmacyQuery.Contracts.ProductCategory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_PharmacyQuery
+{
+    public class MenuModel
+    {
+        //public List<ArticleCategoryQueryModel> ArticleCategories { get; set; }
+        public List<ProductCategoryQueryModel> ProductCategories { get; set; }
+        //public List<TitleCategoryQueryModel> TitleCategories { get; set; }
+    }
+}
---
+++ Pharmacy/ServiceHost/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using _01_PharmacyQuery;
+using _01_PharmacyQuery.Contracts.ProductCategory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        //private readonly IProductCategoryQuery _productCategoryQuery;
+        //private readonly IArticleCategoryQuery _articleCategoryQuery;
+        //public MenuViewComponent(IProductCategoryQuery productCategoryQuery, IArticleCategoryQuery articleCategoryQuery)
+        //{
+        //    _articleCategoryQuery = articleCategoryQuery;
+        //    _productCategoryQuery = productCategoryQuery;
+        //}
+
+        private readonly IProductCategoryQuery _productCategoryQuery;
+        public MenuViewComponent(IProductCategoryQuery productCategoryQuery)
+        {
+            _productCategoryQuery = productCategoryQuery;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var result = new MenuModel
+            {
+                //ArticleCategories = _articleCategoryQuery.GetArticleCategories(),
+                ProductCategories = _productCategoryQuery.GetProductCategories()
+            };
+            return View(result);
+        }
+    }
+}
